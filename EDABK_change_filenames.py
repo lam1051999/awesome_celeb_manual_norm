1d5ea8aa4a780c46337d47fee86ed345c8cc7aca
lam1051999 <lam1051999@gmail.com>
2021-04-22 21:21:49 +0700

    fix change filenames

--- EDABK_change_filenames.py
+++ EDABK_change_filenames.py
@@ -16,8 +16,7 @@ def main() -> None:
         check_path_exist(opt.our_label)
 
         # image attributes
-        spoof_type = 1
-        illumination_condition = 1
+        spoof_type = 3
         environment = 1
 
         live_label = 0
@@ -29,18 +28,38 @@ def main() -> None:
             A_dir = os.path.join(data, A_x)
             for label_x in os.listdir(A_dir):
                 label_dir = os.path.join(A_dir, label_x)
-                image_paths = os.listdir(label_dir)
-                image_paths.sort()
-                for i, image_path in tqdm(enumerate(image_paths)):
-                    new_name = "{}{}{:06d}.{}".format(A_x[1:], live_label if label_x == "live" else spoof_label, i, image_path.split(".")[-1])
-                    temp = os.path.join("Data", type_dir, A_x, label_x, new_name)
-                    new_name = os.path.join(label_dir, new_name)
-                    if not os.path.exists(new_name):
-                        os.rename(os.path.join(label_dir, image_path), new_name)
-                
-                    if temp not in type_label:
-                        type_label[temp] = [i%5, spoof_type, illumination_condition, environment, 1 if label_x == "spoof" else 0]
-        
+                if label_x == "live":
+                    image_paths = os.listdir(label_dir)
+                    image_paths.sort()
+                    for i, image_path in tqdm(enumerate(image_paths)):
+                        new_name = "{}{}{:06d}.{}".format(A_x[1:], live_label if label_x == "live" else spoof_label, i, image_path.split(".")[-1])
+                        temp = os.path.join("Data", type_dir, A_x, label_x, new_name)
+                        new_name = os.path.join(label_dir, new_name)
+                        if not os.path.exists(new_name):
+                            os.rename(os.path.join(label_dir, image_path), new_name)
+                    
+                        if temp not in type_label:
+                            type_label[temp] = [0, 0, 0, 0, live_label]
+                else:
+                    illuminations = os.listdir(label_dir)
+                    for illumination in illuminations:
+                        image_paths = os.listdir(os.path.join(label_dir, illumination))
+                        image_paths.sort()
+                        for i, image_path in tqdm(enumerate(image_paths)):
+                            new_name = "{}{}{:06d}.{}".format(A_x[1:], live_label if label_x == "live" else spoof_label, i, image_path.split(".")[-1])
+                            temp = os.path.join("Data", type_dir, A_x, label_x, illumination, new_name)
+                            new_name = os.path.join(label_dir, illumination, new_name)
+                            if not os.path.exists(new_name):
+                                os.rename(os.path.join(label_dir, illumination, image_path), new_name)
+
+                            if temp not in type_label:
+                                if illumination == "other":
+                                    type_label[temp] = [0, 1, 1, 1, 1]
+                                elif illumination == "dark":
+                                    type_label[temp] = [i%5, spoof_type, 4, environment, spoof_label]
+                                else:
+                                    type_label[temp] = [i%5, spoof_type, 2, environment, spoof_label]
+
         type_label = shuffle_dictionary(type_label)
         if type_dir == "train":
             with open(os.path.join(opt.our_label, 'train.json'), 'w') as outfile:
