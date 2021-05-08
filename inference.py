63a2e5b1fe24f8c43daa0cc7871675d4348ca44f
lam1051999 <lam1051999@gmail.com>
2021-05-08 13:41:32 +0700

    add insightface to inference

--- inference.py
+++ inference.py
@@ -8,6 +8,10 @@ import numpy as np
 import cv2
 import glob
 import math
+import sys
+
+from retinaface import RetinaFace
+
 
 # def inference(**kwargs):
 #     path = kwargs["image"]
@@ -70,23 +74,95 @@ import math
 
 #     fopen.close()
 
+# def inference(**kwargs):
+#     images = kwargs["images"]
+#     crop_threshold = kwargs["crop_threshold"]
+#     spoof_threshold = kwargs["spoof_threshold"]
+
+#     YOLO_IMG_WIDTH = opt.YOLO_IMG_WIDTH
+#     YOLO_IMG_HEIGHT = opt.YOLO_IMG_HEIGHT
+#     CONFIDENCE_THRESHOLD = 0.2
+#     NMS_THRESHOLD = 0.4
+#     PADDING = 20
+#     net = cv2.dnn.readNet(opt.yolo_weights_path, opt.yolo_config_path)
+#     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+#     model_yolo = cv2.dnn_DetectionModel(net)
+#     model_yolo.setInputParams(size=(YOLO_IMG_WIDTH, YOLO_IMG_HEIGHT), scale=1/255, swapRB=True)
+  
+#     # load crop model
+    
+#     color = (0, 0, 255)
+#     thickness = 3
+#     font = cv2.FONT_HERSHEY_SIMPLEX
+#     fontScale = 3
+#     # load model
+#     pths = glob.glob('checkpoints-photo-celeb/%s/*.pth' % (opt.model))
+#     pths.sort(key=os.path.getmtime, reverse=True)
+#     print(pths)
+
+#     opt.load_model_path = pths[0]
+#     model = getattr(models, opt.model)().eval()
+#     assert os.path.exists(opt.load_model_path)
+#     if opt.load_model_path:
+#         model.load(opt.load_model_path)
+#     if opt.use_gpu:
+#         model.cuda()
+#     model.train(False)
+
+#     count = 0
+#     for image in os.listdir(images):
+#         if image.split(".")[-1] == "jpg" or image.split(".")[-1] == "png" or image.split(".")[-1] == "jpeg" or image.split(".")[-1] == "JPG" or image.split(".")[-1] == "PNG" or image.split(".")[-1] == "JPEG":
+#             path = os.path.join(images, image)
+#             im = cv2.imread(path)
+#             (h, w) = im.shape[:2]
+#             if h < 800 or w < 800:
+#                 fontScale = 1
+#                 thickness = 1
+#             classes, scores, boxes = model_yolo.detect(im, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+
+#             for (classid, score, box) in zip(classes, scores, boxes):
+#                 if score >= float(crop_threshold):
+#                     (startX, startY, endX, endY) = (box[0], box[1], box[0] + box[2], box[1] + box[3])
+#                     if startX <= w and endX <= w and startY <= h and endY <= h:
+#                         (startX, startY, endX, endY) = (startX - PADDING, startY - PADDING, endX + PADDING, endY + PADDING)
+#                         if startX < 0:
+#                             startX = 0
+#                         if startY < 0:
+#                             startY = 0
+#                         face = im[startY:endY, startX:endX]
+
+#                         if 0 not in face.shape:
+#                             face = cv2.resize(face, (opt.image_size, opt.image_size))
+#                             face = face/255
+#                             face = np.transpose(np.array(face, dtype=np.float32), (2, 0, 1))
+#                             face = face[np.newaxis, :]
+#                             face = torch.FloatTensor(face)
+#                             with torch.no_grad():
+#                                 if opt.use_gpu:
+#                                     face = face.cuda()
+#                                 outputs = model(face)
+#                                 outputs = torch.softmax(outputs, dim=-1)
+#                                 preds = outputs.to('cpu').numpy()
+#                                 attack_prob = preds[:, opt.ATTACK]
+#                                 if sum(attack_prob) >= float(spoof_threshold):
+#                                     count += 1
+#                                 im = cv2.putText(im, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5 if startX - 5 > 0 else startX + 5, startY - 5 if startY - 5 > 0 else startY + 5), font, fontScale, color, thickness, cv2.LINE_AA)
+#                                 im = cv2.rectangle(im, (startX, startY), (endX, endY), color, thickness)
+#                                 cv2.imwrite(path.split(".")[0]+"_evaluated." + path.split(".")[1], im)
+
+#     print("Number of spoof faces in the images in {} is: {}".format(images, count))
+
 def inference(**kwargs):
     images = kwargs["images"]
-    crop_threshold = kwargs["crop_threshold"]
     spoof_threshold = kwargs["spoof_threshold"]
 
-    YOLO_IMG_WIDTH = opt.YOLO_IMG_WIDTH
-    YOLO_IMG_HEIGHT = opt.YOLO_IMG_HEIGHT
-    CONFIDENCE_THRESHOLD = 0.2
-    NMS_THRESHOLD = 0.4
-    PADDING = 20
-    net = cv2.dnn.readNet(opt.yolo_weights_path, opt.yolo_config_path)
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-    model_yolo = cv2.dnn_DetectionModel(net)
-    model_yolo.setInputParams(size=(YOLO_IMG_WIDTH, YOLO_IMG_HEIGHT), scale=1/255, swapRB=True)
-  
     # load crop model
+
+    thresh = 0.8
+    gpuid = 0
+    detector = RetinaFace('./model/R50', 0, gpuid, 'net3')
+    PADDING = 10
     
     color = (0, 0, 255)
     thickness = 3
@@ -110,42 +186,58 @@ def inference(**kwargs):
     for image in os.listdir(images):
         if image.split(".")[-1] == "jpg" or image.split(".")[-1] == "png" or image.split(".")[-1] == "jpeg" or image.split(".")[-1] == "JPG" or image.split(".")[-1] == "PNG" or image.split(".")[-1] == "JPEG":
             path = os.path.join(images, image)
-            im = cv2.imread(path)
-            (h, w) = im.shape[:2]
-            if h < 800 or w < 800:
+            img = cv2.imread(path)
+
+            scales = [1024, 1980]
+            im_shape = img.shape
+            target_size = scales[0]
+            max_size = scales[1]
+            im_size_min = np.min(im_shape[0:2])
+            im_size_max = np.max(im_shape[0:2])
+            im_scale = float(target_size) / float(im_size_min)
+            if np.round(im_scale * im_size_max) > max_size:
+                im_scale = float(max_size) / float(im_size_max)
+
+            scales = [im_scale]
+            flip = False
+            faces, landmarks = detector.detect(img,
+                                            thresh,
+                                            scales=scales,
+                                            do_flip=flip)
+
+
+            if img.shape[0] < 800 or img.shape[1] < 800:
                 fontScale = 1
                 thickness = 1
-            classes, scores, boxes = model_yolo.detect(im, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
-            for (classid, score, box) in zip(classes, scores, boxes):
-                if score >= float(crop_threshold):
-                    (startX, startY, endX, endY) = (box[0], box[1], box[0] + box[2], box[1] + box[3])
-                    if startX <= w and endX <= w and startY <= h and endY <= h:
-                        (startX, startY, endX, endY) = (startX - PADDING, startY - PADDING, endX + PADDING, endY + PADDING)
-                        if startX < 0:
-                            startX = 0
-                        if startY < 0:
-                            startY = 0
-                        face = im[startY:endY, startX:endX]
+            if faces is not None:
+                for i in range(faces.shape[0]):
+                    box = faces[i].astype(np.int)
+                    (startX, startY, endX, endY) = (box[0] - 10, box[1] - 10, box[2] + 10, box[3] + 10)
+                    startX = max(0, startX)
+                    startY = max(0, startY)
+                    endX = min(endX, im_shape[1])
+                    endY = min(endY, im_shape[0])
+                    crop_face = img[startY:endY, startX:endX]
 
-                        if 0 not in face.shape:
-                            face = cv2.resize(face, (opt.image_size, opt.image_size))
-                            face = face/255
-                            face = np.transpose(np.array(face, dtype=np.float32), (2, 0, 1))
-                            face = face[np.newaxis, :]
-                            face = torch.FloatTensor(face)
-                            with torch.no_grad():
-                                if opt.use_gpu:
-                                    face = face.cuda()
-                                outputs = model(face)
-                                outputs = torch.softmax(outputs, dim=-1)
-                                preds = outputs.to('cpu').numpy()
-                                attack_prob = preds[:, opt.ATTACK]
-                                if sum(attack_prob) >= float(spoof_threshold):
-                                    count += 1
-                                im = cv2.putText(im, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5 if startX - 5 > 0 else startX + 5, startY - 5 if startY - 5 > 0 else startY + 5), font, fontScale, color, thickness, cv2.LINE_AA)
-                                im = cv2.rectangle(im, (startX, startY), (endX, endY), color, thickness)
-                                cv2.imwrite(path.split(".")[0]+"_evaluated." + path.split(".")[1], im)
+                    if 0 not in crop_face.shape:
+                        crop_face = cv2.resize(crop_face, (opt.image_size, opt.image_size))
+                        crop_face = crop_face/255
+                        crop_face = np.transpose(np.array(crop_face, dtype=np.float32), (2, 0, 1))
+                        crop_face = crop_face[np.newaxis, :]
+                        crop_face = torch.FloatTensor(crop_face)
+                        with torch.no_grad():
+                            if opt.use_gpu:
+                                crop_face = crop_face.cuda()
+                            outputs = model(crop_face)
+                            outputs = torch.softmax(outputs, dim=-1)
+                            preds = outputs.to('cpu').numpy()
+                            attack_prob = preds[:, opt.ATTACK]
+                            if sum(attack_prob) >= float(spoof_threshold):
+                                count += 1
+                            img = cv2.putText(img, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5 if startX - 5 > 0 else startX + 5, startY - 5 if startY - 5 > 0 else startY + 5), font, fontScale, color, thickness, cv2.LINE_AA)
+                            img = cv2.rectangle(img, (startX, startY), (endX, endY), color, thickness)
+                            cv2.imwrite(path.split(".")[0]+"_evaluated." + path.split(".")[1], img)
 
     print("Number of spoof faces in the images in {} is: {}".format(images, count))
 
--- video_inference.py
+++ video_inference.py
@@ -8,6 +8,10 @@ import numpy as np
 import cv2
 import glob
 import math
+import sys
+
+from retinaface import RetinaFace
+
 
 # def video_inference(**kwargs):
 #     path = kwargs['video']
@@ -83,20 +87,92 @@ import math
 #     cap.release()
 #     cv2.destroyAllWindows()
 
+# def video_inference(**kwargs):
+#     path = kwargs['video']
+
+#     # load crop model
+#     YOLO_IMG_WIDTH = opt.YOLO_IMG_WIDTH
+#     YOLO_IMG_HEIGHT = opt.YOLO_IMG_HEIGHT
+#     CONFIDENCE_THRESHOLD = 0.2
+#     NMS_THRESHOLD = 0.4
+#     PADDING = 20
+#     net = cv2.dnn.readNet(opt.yolo_weights_path, opt.yolo_config_path)
+#     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+#     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+#     model_yolo = cv2.dnn_DetectionModel(net)
+#     model_yolo.setInputParams(size=(YOLO_IMG_WIDTH, YOLO_IMG_HEIGHT), scale=1/255, swapRB=True)
+
+#     # initialize video
+#     cap = cv2.VideoCapture(path)
+#     color = (255, 0, 0)
+#     thickness = 2 
+#     font = cv2.FONT_HERSHEY_SIMPLEX
+#     fontScale = 1
+
+#     # load model
+#     pths = glob.glob('checkpoints/%s/*.pth' % (opt.model))
+#     pths.sort(key=os.path.getmtime, reverse=True)
+#     print(pths)
+
+#     opt.load_model_path = pths[0]
+#     model = getattr(models, opt.model)().eval()
+#     assert os.path.exists(opt.load_model_path)
+#     if opt.load_model_path:
+#         model.load(opt.load_model_path)
+#     if opt.use_gpu:
+#         model.cuda()
+#     model.train(False)
+#     while(True):
+#         ret, im = cap.read()    
+#         if not ret:
+#             print('==> Done processing!!!')
+#             cv2.waitKey(1000)
+#             break
+#         (h, w) = im.shape[:2]
+#         classes, scores, boxes = model_yolo.detect(im, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+
+#         for (classid, score, box) in zip(classes, scores, boxes):
+#             if score >= 0.5:
+#                 (startX, startY, endX, endY) = (box[0], box[1], box[0] + box[2], box[1] + box[3])
+#                 if startX <= w and endX <= w and startY <= h and endY <= h:
+#                     (startX, startY, endX, endY) = (startX - PADDING, startY - PADDING, endX + PADDING, endY + PADDING)
+#                     if startX < 0:
+#                         startX = 0
+#                     if startY < 0:
+#                         startY = 0
+#                     face = im[startY:endY, startX:endX]
+#                     if 0 not in face.shape:
+#                         im = cv2.rectangle(im, (startX, startY), (endX, endY), color, thickness)
+#                         face = cv2.resize(face, (opt.image_size, opt.image_size))
+#                         face = face/255
+#                         face = np.transpose(np.array(face, dtype=np.float32), (2, 0, 1))
+#                         face = face[np.newaxis, :]
+#                         face = torch.FloatTensor(face)
+#                         with torch.no_grad():
+#                             if opt.use_gpu:
+#                                 face = face.cuda()
+#                             outputs = model(face)
+#                             outputs = torch.softmax(outputs, dim=-1)
+#                             preds = outputs.to('cpu').numpy()
+#                             attack_prob = preds[:, opt.ATTACK]
+#                             im = cv2.putText(im, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5, startY - 5), font, fontScale, color, thickness, cv2.LINE_AA)
+
+
+#         cv2.imshow('frame',im)
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+
 def video_inference(**kwargs):
     path = kwargs['video']
 
     # load crop model
-    YOLO_IMG_WIDTH = opt.YOLO_IMG_WIDTH
-    YOLO_IMG_HEIGHT = opt.YOLO_IMG_HEIGHT
-    CONFIDENCE_THRESHOLD = 0.2
-    NMS_THRESHOLD = 0.4
-    PADDING = 20
-    net = cv2.dnn.readNet(opt.yolo_weights_path, opt.yolo_config_path)
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-    model_yolo = cv2.dnn_DetectionModel(net)
-    model_yolo.setInputParams(size=(YOLO_IMG_WIDTH, YOLO_IMG_HEIGHT), scale=1/255, swapRB=True)
+    thresh = 0.8
+    gpuid = 0
+    detector = RetinaFace('./model/R50', 0, gpuid, 'net3')
+    PADDING = 10    
 
     # initialize video
     cap = cv2.VideoCapture(path)
@@ -106,7 +182,7 @@ def video_inference(**kwargs):
     fontScale = 1
 
     # load model
-    pths = glob.glob('checkpoints/%s/*.pth' % (opt.model))
+    pths = glob.glob('checkpoints-photo-celeb/%s/*.pth' % (opt.model))
     pths.sort(key=os.path.getmtime, reverse=True)
     print(pths)
 
@@ -119,42 +195,56 @@ def video_inference(**kwargs):
         model.cuda()
     model.train(False)
     while(True):
-        ret, im = cap.read()    
+        ret, img = cap.read()    
         if not ret:
             print('==> Done processing!!!')
             cv2.waitKey(1000)
             break
-        (h, w) = im.shape[:2]
-        classes, scores, boxes = model_yolo.detect(im, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
-        for (classid, score, box) in zip(classes, scores, boxes):
-            if score >= 0.5:
-                (startX, startY, endX, endY) = (box[0], box[1], box[0] + box[2], box[1] + box[3])
-                if startX <= w and endX <= w and startY <= h and endY <= h:
-                    (startX, startY, endX, endY) = (startX - PADDING, startY - PADDING, endX + PADDING, endY + PADDING)
-                    if startX < 0:
-                        startX = 0
-                    if startY < 0:
-                        startY = 0
-                    face = im[startY:endY, startX:endX]
-                    if 0 not in face.shape:
-                        im = cv2.rectangle(im, (startX, startY), (endX, endY), color, thickness)
-                        face = cv2.resize(face, (opt.image_size, opt.image_size))
-                        face = face/255
-                        face = np.transpose(np.array(face, dtype=np.float32), (2, 0, 1))
-                        face = face[np.newaxis, :]
-                        face = torch.FloatTensor(face)
-                        with torch.no_grad():
-                            if opt.use_gpu:
-                                face = face.cuda()
-                            outputs = model(face)
-                            outputs = torch.softmax(outputs, dim=-1)
-                            preds = outputs.to('cpu').numpy()
-                            attack_prob = preds[:, opt.ATTACK]
-                            im = cv2.putText(im, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5, startY - 5), font, fontScale, color, thickness, cv2.LINE_AA)
+        scales = [1024, 1980]
+        im_shape = img.shape
+        target_size = scales[0]
+        max_size = scales[1]
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+        im_scale = float(target_size) / float(im_size_min)
+        if np.round(im_scale * im_size_max) > max_size:
+            im_scale = float(max_size) / float(im_size_max)
 
+        scales = [im_scale]
+        flip = False
+        faces, landmarks = detector.detect(img,
+                                        thresh,
+                                        scales=scales,
+                                        do_flip=flip)
 
-        cv2.imshow('frame',im)
+        if faces is not None:
+            for i in range(faces.shape[0]):
+                box = faces[i].astype(np.int)
+                (startX, startY, endX, endY) = (box[0] - 10, box[1] - 10, box[2] + 10, box[3] + 10)
+                startX = max(0, startX)
+                startY = max(0, startY)
+                endX = min(endX, im_shape[1])
+                endY = min(endY, im_shape[0])
+                crop_face = img[startY:endY, startX:endX]
+
+                if 0 not in crop_face.shape:
+                    img = cv2.rectangle(img, (startX, startY), (endX, endY), color, thickness)
+                    crop_face = cv2.resize(crop_face, (opt.image_size, opt.image_size))
+                    crop_face = crop_face/255
+                    crop_face = np.transpose(np.array(crop_face, dtype=np.float32), (2, 0, 1))
+                    crop_face = crop_face[np.newaxis, :]
+                    crop_face = torch.FloatTensor(crop_face)
+                    with torch.no_grad():
+                        if opt.use_gpu:
+                            crop_face = crop_face.cuda()
+                        outputs = model(crop_face)
+                        outputs = torch.softmax(outputs, dim=-1)
+                        preds = outputs.to('cpu').numpy()
+                        attack_prob = preds[:, opt.ATTACK]
+                        img = cv2.putText(img, "Spoof {:.2f}".format(sum(attack_prob)), (startX - 5, startY - 5), font, fontScale, color, thickness, cv2.LINE_AA)
+
+        cv2.imshow('frame',img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
